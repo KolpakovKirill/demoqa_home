e32d8a6a66dcadc1202fa736f132f8b951ac6e1c
Kirill <kolpakov7@yandex.ru>
2023-09-15 14:07:38 +0300

    first commit 1

--- conftest.py
+++ conftest.py
@@ -7,3 +7,8 @@ def browser():
     driver = webdriver.Chrome()
     yield driver                  # примерно как return но не завершает функцию
     driver.quit()
+
+
+#@pytest.fixture(scope="session")     # Декоратор говорит что эта функция является фикстурой
+#def browser():
+    ###driver.quit()
