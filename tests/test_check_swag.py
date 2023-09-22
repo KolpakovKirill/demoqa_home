ff0eac1afa94922ae819844fe429370fd35e5666
Kirill <kolpakov7@yandex.ru>
2023-09-22 11:55:37 +0300

    5 commit

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -7,12 +7,13 @@ def test_check_elements_icon(browser):
     assert page_sauce_demo.exist_icon()
 
 
-def test_1_check_elements_user_name(browser):
+def test_check_elements_user_name(browser):
     page_sauce_demo = SwagLabs(browser)
     page_sauce_demo.visit()
     assert page_sauce_demo.find_element('#user-name')
 
-def test_2_check_elements_password(browser):
+
+def test_heck_elements_password(browser):
     page_sauce_demo = SwagLabs(browser)
     page_sauce_demo.visit()
     assert page_sauce_demo.find_element('#password')
